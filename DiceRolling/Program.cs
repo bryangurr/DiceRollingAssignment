6b879c457640d000b9257477a227fbd771a5931f
bryangurr <150076870+bryangurr@users.noreply.github.com>
2025-01-17 17:01:39 -0700

    Initial commit

---
+++ DiceRolling/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using DiceRolling;
+
+internal class Program
+{
+    public static void Main(string[] args)
+    {
+        int numRolls = 100;
+        string starString = "";
+        Console.WriteLine("Welcome to the dice throwing simulator! \n");
+        Console.WriteLine("How many dice rolls would you like to simulate?");
+        numRolls = Convert.ToInt32(Console.ReadLine());
+
+        int[] results = new int[numRolls];
+
+        Console.WriteLine("Perfect! We will roll " + numRolls + " dice.");
+        rollDice rollDice = new rollDice();
+        results = rollDice.Roll(numRolls, 2, 6);
+
+        Dictionary<int, int> rollCounts = new Dictionary<int, int>();
+
+        for (int i = 0; i < results.Length; i++)
+        {
+            // Console.WriteLine(results[i]);
+            if (rollCounts.ContainsKey(results[i]))
+            {
+                rollCounts[results[i]]++;
+            }
+            else
+            {
+                rollCounts.Add(results[i], 1);
+            }
+        }
+
+        for (int key = 2; key < 13; key++)
+        {
+            if (rollCounts.ContainsKey(key))
+            {
+                for (int star = 0; star < 100 * rollCounts[key] / numRolls; star++)
+                {
+                    starString += "*";
+                }
+                Console.WriteLine($"{key}: {starString}");
+                starString = "";
+            }
+            else
+            {
+                Console.WriteLine($"{key}: ");
+            }
+        }
+    }
+}
---
+++ DiceRolling/rollDice.cs
@@ -0,0 +1,24 @@
+﻿namespace DiceRolling;
+
+public class rollDice
+{
+    public static int[] Roll(int numRolls, int numDice, int numSides)
+    {
+        int[] results = new int[numRolls];
+        Random rnd = new Random();
+        int value = 0;
+        int total = 0;
+        for(int i= 0; i < numRolls; i++)
+        {
+            total = 0;
+            for (int j = 0; j < numDice; j++)
+            {
+                value = rnd.Next(1, numSides + 1);
+                total += value;
+            }
+            results[i] = total;
+        }
+        
+        return results;
+    }
+}
